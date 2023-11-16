11f77b36128cc9841618a73daa9f5502b8f048f8
Zintu paul <jintupaul45@gmail.com>
2023-11-16 21:34:47 +0600

    install some dependencies

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const cors = require("cors")
+require('dotenv').config()
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.EDUCATION_COURSE}:${process.env.EDUCATION_PASS}@cluster0.t6zznhm.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run(){
+  try{
+      const coursesCollection = 
+  }
+  finally{}
+}
+run().catch(error => console.error(error))
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
